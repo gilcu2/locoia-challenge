cf655d22e6c691a9cf7fc627b25adb4ac47c318a
Reynaldo Gil <gilcu2@gmail.com>
2025-03-05 06:13:57 +0100

    pin test ok

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,17 +20,3 @@ def client(app):
 def runner(app):
     return app.test_cli_runner()
 
-def Given(label: str):
-    print(f"Given:\n {label}")
-
-
-def When(label: str):
-    print(f"When:\n {label}")
-
-
-def Then(label: str):
-    print(f"Then:\n {label}")
-
-
-def And(label: str):
-    print(f"And:\n {label}")
--- tests/gistapi_unit_test.py
+++ tests/gistapi_unit_test.py
@@ -1,3 +1,11 @@
-def test_request_example(client):
+from bdd_helper import *
+
+
+def test_ping(client):
+    Given("client")
+
+    When("get ping")
     response = client.get("/ping")
+
+    Then("it is expected")
     assert response.data == b"pong"

64ff792a6c651fb2a3d69aedb0319fc06133d5f9
Reynaldo Gil <gilcu2@gmail.com>
2025-03-06 11:38:13 +0100

    added coverage report

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,4 +6,4 @@ TIMEOUT="--timeout=60 --timeout_method=signal"
 
 TEST_DIRS="tests"
 
-pytest -ra $TIMEOUT --durations=0 -n $PARALLELISM --dist loadgroup $TEST_DIRS
+pytest -ra $TIMEOUT --durations=0 -n $PARALLELISM --dist loadgroup --cov=gistapi $TEST_DIRS
